63513d009c8413070f0329533f41d0cc5499b2a2
Lama <cyprien.puljiz@gmail.com>
2020-07-02 00:41:23 +0200

    test56

---
+++ Site/PHP/menus.php
@@ -0,0 +1,14 @@
+<nav id="menu">
+  <ul > 
+  	<li><a href="../index.html" class="Home">Home</a></li>
+    <li><a href="../Live/Live.html" class="Home">Voir le live</a></li>
+    <li class="deroulant"><a href="#">Star Citizen &ensp;</a>
+      <ul class="sous">
+        <li><a href="../SC/SC.html">Info Star Citizen</a></li>
+        <li><a href="">Spartan Corp</a></li>
+        <li><a href="">Operation Artemis</a></li>
+        <li><a href="">Operation Idris</a></li>
+      </ul>
+    <li><a href="../A propos/A_Propos.html">A propos</a></li>
+  </ul>
+</nav>
